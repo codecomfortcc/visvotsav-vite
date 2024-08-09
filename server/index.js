18777cf029837f788c1ecaf3179b4a3b6cb4b494
Masani Yasovardhan <yasovardhanmasani@gmail.com>
2024-08-10 00:55:07 +0530

    cors setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://visvotsav.vercel.app"],
+    origin: [ "https://visvotsav.vercel.app"],
     credentials: true,
   })
 );
